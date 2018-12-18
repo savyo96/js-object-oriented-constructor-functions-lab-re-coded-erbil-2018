987d8abd212cb36b0e9c66df479848a58378987c
delightful-map-2010 <savyo.yohana@yahoo.com>
2018-12-18 15:10:26 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,17 @@
+let Scooter = {
+  year: 20,
+  color: 'red',
+  model: '2018'
+}
+ 
+let Driver = {
+  name: 'fadi',
+  age: 28,
+  experience: 'butifull'
+}
+
+let PickupLocation = {
+  address: 'alfaris-streed',
+  city: 'qaragush',
+
+}
